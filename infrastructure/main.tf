678bf863b3f0a2f2c2008b159e1d95d23d714753
Kaleb Lape <kaleb.lape@metova.com>
2017-06-19 08:27:00 -0500

    Create test cluster.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-west-2"
+  region     = "${var.aws_region}"
 }
 
 terraform {
   backend "s3" {
-    bucket = "spec-queue-tfstate"
+    bucket = "spec-queue-infrastructure-tfstate"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
