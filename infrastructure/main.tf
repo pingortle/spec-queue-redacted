4cff25fbcccfba7c8545ba15dddc0ce8c41b49ac
Kaleb Lape <kaleb.lape@metova.com>
2017-06-20 16:45:49 -0500

    Increase master server capacity.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,25 +18,10 @@ module "stack" {
   keypair = "spec-queue"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami                         = "${data.aws_ami.ubuntu.id}"
-  instance_type               = "t2.micro"
+  ami                         = "ami-ad4b33bb"
+  instance_type               = "m4.large"
+  monitoring                  = true
   associate_public_ip_address = true
   key_name                    = "spec-queue"
   vpc_security_group_ids      = ["${module.stack.public_sg}", "${module.stack.private_sg}"]
