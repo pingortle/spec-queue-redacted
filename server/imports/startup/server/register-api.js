e73b53c8ba695a2e32f687cbd7887af7a7c6a71d
Kaleb Lape <kaleb.lape@metova.com>
2017-06-26 02:03:32 -0500

    Load examples publications.

--- server/imports/startup/server/register-api.js
+++ server/imports/startup/server/register-api.js
@@ -4,3 +4,5 @@ import '../../api/builds/methods.js'
 import '../../api/builds/server/publications.js'
 
 import  '../../api/jobs/server/publications.js'
+
+import '../../api/examples/server/publications.js'
