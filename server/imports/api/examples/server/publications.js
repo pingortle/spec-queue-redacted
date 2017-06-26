63995b2707aa0e367a47e2205a7fc100ad984846
Kaleb Lape <kaleb.lape@metova.com>
2017-06-26 09:18:11 -0500

    Include buildId in published examples.

--- server/imports/api/examples/server/publications.js
+++ server/imports/api/examples/server/publications.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import { Examples } from '../examples.js'
 
 Meteor.publish('examples.forBuild', function ({ buildId }) {
-  return Examples.find({ buildId }, { fields: { _id: 1, status: 1 } })
+  return Examples.find({ buildId }, { fields: { _id: 1, status: 1, buildId: 1 } })
 })
 
 Meteor.publish('examples.withDetails', function (ids) {
