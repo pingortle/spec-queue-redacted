4e64904979710d5314b66a63ef360cee9741019e
Kaleb Lape <kaleb.lape@metova.com>
2017-06-29 15:24:30 -0500

    Implement WIP CLI build waiter.

--- cli/index.js
+++ cli/index.js
@@ -4,26 +4,37 @@ const ddp = new DDP({
   host: process.env.METEOR_HOST || "127.0.0.1",
   port: process.env.METEOR_PORT || 3000,
   autoReconnect : true,
-  useSockJs: true,
+  useSockJs: true
 })
 
-ddp.connect(function (error) {
-  console.error(error)
+let buildId = null
 
-  ddp.call('builds.createJob', [], function (error, buildId) {
-    throwIf(error)
+const buildObserver = ddp.observe('builds')
+buildObserver.changed = (id, oldFields, clearedFields, newFields) => {
+  const status = newFields.status
+  if (!status) return;
+
+  console.log(`Build result: ${status}`)
+  if (status === 'passed') {
+    process.exit(0)
+  } else {
+    process.exit(1)
+  }
+}
+
+ddp.connect(function (error, isReconnected) {
+  console.log('connected, waiting for build results...')
+
+  if (buildId) ddp.subscribe('builds.one', [buildId])
+
+  if (error || isReconnected) {
+    if (error) console.error(error)
+    return;
+  }
+
+  ddp.call('builds.createJob', [{ gitCommitId: process.env.GIT_COMMIT }], function (error, newBuildId) {
+    buildId = newBuildId
+    ddp.subscribe('builds.one', [buildId])
     console.log(`running build: ${buildId}`)
-
-    const buildSubscription = ddb.subscribe('builds.one', [buildId])
-
-    const buildObserver = ddp.observe('builds')
-    buildObserver.changed = (id, oldFields, clearedFields, newFields) => {
-      const status = newFields.status
-      if (!status || status === 'passed') {
-        process.exit(0)
-      } else {
-        process.exit(1)
-      }
-    }
   })
 })
