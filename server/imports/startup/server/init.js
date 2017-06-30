262847cc4450fe1a35de06e94118bc3e5bfa09e1
Kaleb Lape <kaleb.lape@metova.com>
2017-06-30 11:39:27 -0500

    Add mongo indices.

--- server/imports/startup/server/init.js
+++ server/imports/startup/server/init.js
@@ -1,7 +1,13 @@
 import { Meteor } from 'meteor/meteor'
 import { DefaultJobQueue } from '../../api/jobs/jobs.js'
+import { Examples } from '../../api/examples/examples.js'
 
 Meteor.startup(() => {
+  DefaultJobQueue._ensureIndex({ 'data.buildId': 1 })
+  DefaultJobQueue._ensureIndex({ 'data.buildId': 1, status: 1 })
+  Examples._ensureIndex({ buildId: 1 })
+  Examples._ensureIndex({ buildId: 1, status: 1 })
+
   DefaultJobQueue.startJobServer()
   DefaultJobQueue.setLogStream(process.stdout)
 })
