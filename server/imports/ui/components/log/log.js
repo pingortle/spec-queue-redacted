180c528014ac6448fc1380bb39b9067973382aaa
Kaleb Lape <kaleb.lape@metova.com>
2017-07-06 13:34:11 -0500

    Auto refresh log view.

--- server/imports/ui/components/log/log.js
+++ server/imports/ui/components/log/log.js
@@ -1,20 +1,31 @@
 import { Meteor } from 'meteor/meteor'
+import { reactiveInterval } from 'meteor/teamgrid:reactive-interval'
 import { DefaultJobQueue } from '../../../api/jobs/jobs.js'
 import './log.html'
 
 Template.log.viewmodel({
   loaded: false,
+  interval: 1000,
+  autoReload: false,
   log: [],
   id() { return this.resource()._id },
   prettyLog() { return this.log().map(log => log.message).join('') },
+  autoReloadClass() { return this.autoReload() ? 'btn-success' : 'btn-default' },
   fetchLog() {
     Meteor.call('jobs.default.fetchLog', { jobId: this.id() }, (error, result) => {
-      console.log(error, result)
+      if (error) return console.log(error)
+
+      const delta = result.log.length - this.log().length
+      console.log(`loaded ${delta} new logs`)
       this.log(result.log)
       this.loaded(true)
     })
   },
-  onCreated() {
+  onRendered() {
     this.fetchLog()
+  },
+  autorun() {
+    reactiveInterval(this.interval())
+    if (this.autoReload()) this.fetchLog()
   }
 })
