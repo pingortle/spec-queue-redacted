069f485387ba9a5d9ce32fc619b0030ec4579a86
Kaleb Lape <kaleb.lape@metova.com>
2017-07-06 16:05:27 -0500

    Shore up log view.

--- server/imports/ui/components/log/log.js
+++ server/imports/ui/components/log/log.js
@@ -5,11 +5,19 @@ import './log.html'
 
 Template.log.viewmodel({
   loaded: false,
-  interval: 1000,
+  interval: 5000,
   autoReload: false,
+  prettyMode: true,
   log: [],
   id() { return this.resource()._id },
   prettyLog() { return this.log().map(log => log.message).join('') },
+  detailedLog() {
+    const log = this.log()
+    return log.map((entry, index) => {
+      entry.nextEntry = log[index + 1]
+      return entry
+    })
+  },
   autoReloadClass() { return this.autoReload() ? 'btn-success' : 'btn-default' },
   fetchLog() {
     Meteor.call('jobs.default.fetchLog', { jobId: this.id() }, (error, result) => {
@@ -27,5 +35,18 @@ Template.log.viewmodel({
   autorun() {
     reactiveInterval(this.interval())
     if (this.autoReload()) this.fetchLog()
+  }
+})
+
+Template.log_entry.viewmodel({
+  durationMilliseconds() {
+    const nextEntry = this.nextEntry()
+    if (nextEntry) return nextEntry.time - this.time()
+
+    reactiveInterval(1000)
+    return new Date() - this.time()
+  },
+  formatDuration(duration) {
+    return `${moment.duration(duration).humanize()} (${moment.duration(duration).asSeconds()} seconds)`
   }
 })
