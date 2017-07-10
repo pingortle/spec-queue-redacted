cea17e98c0116545b53a5d2118a035a59645ce7b
Kaleb Lape <kaleb.lape@metova.com>
2017-07-10 11:44:48 -0500

    Remove "Start Build" from the gui.

--- server/imports/ui/components/builds/builds.js
+++ server/imports/ui/components/builds/builds.js
@@ -5,9 +5,6 @@ import './builds.less'
 
 Template.builds.viewmodel({
   builds: () => Builds.find({}),
-  startBuild: function (event) {
-    Meteor.call('builds.createJob')
-  },
   displayStatus: status => (status || 'running'),
   destroy: function (subject) {
     Meteor.call('builds.destroy', { buildId: subject._id }, (error) => {
